f4d40a2aa8899d7f035a1ac795723d4b6e332d02
antonio.anzivino <antonio.anzivino@csttech.it>
2015-09-17 09:49:27 +0200

    First commit

---
+++ app/src/main/java/org/zighinetto/qrkey/Intents.java
@@ -0,0 +1,10 @@
+package org.zighinetto.qrkey;
+
+/**
+ * Created by antonio.anzivino on 21/08/2015.
+ */
+public interface Intents {
+
+    public static final String INTENT_CAPTURE_COMPLETE="org.zighinetto.qrkey.INTENT_CAPTURE_COMPLETE";
+
+}
